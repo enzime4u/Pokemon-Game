d03afca5b1ec04fb81c51e74807f5051ae225b21
enzime4u <enzime4u@yahoo.com>
2019-07-11 15:49:30 +0300

    Updates

---
+++ precache-manifest.844da995b9adf29ef7303c3702cdbf8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0293a05119ceb83ccf14518af58efb15",
+    "url": "/Pokemon-Game/index.html"
+  },
+  {
+    "revision": "90c37f3c7dfe60b6ad9b",
+    "url": "/Pokemon-Game/static/css/main.68fb77a4.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/Pokemon-Game/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "90c37f3c7dfe60b6ad9b",
+    "url": "/Pokemon-Game/static/js/main.2ef33c4b.chunk.js"
+  },
+  {
+    "revision": "2c41fd14e318821d5a1d",
+    "url": "/Pokemon-Game/static/js/runtime~main.c1e85a01.js"
+  }
+]);
